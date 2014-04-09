0a827595ad407aa8a76fa32e5d6f5e13c4dcbbd2
Henrique César Florêncio <henriquerzo@gmail.com>
2014-04-09 01:44:51 -0300

    Nova tela de login

--- processos/src/index.php
+++ processos/src/index.php
@@ -1,63 +1,79 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-	<head>
-		
-		<link href="css/consulta.css" rel="stylesheet">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/bootstrap-responsive.css" rel="stylesheet">
-		<link href="css/bootstrap-select.min.css" rel="stylesheet">
+<head>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 
-		
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="js/bootstrap-select.min.js"></script>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/index.css" rel="stylesheet">
 
-		
-		<script src="controller/javascriptfunctions.js"></script>
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script src="../libs/bootstrap/js/bootstrap.js"></script>
- 		
-		<meta charset="UTF-8">
-		<title>Consulta Processos</title>
 
-	</head>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script src="../libs/bootstrap/js/bootstrap.js"></script>
 
-<body onload="run_search_processos_usuario()">
+  <script src="controller/javascriptfunctions.js"></script>
+  
+  
+
+  <meta charset="UTF-8">
+  <title>Consulta Processos</title>
+
+</head>
+
+<body>
   <!-- Menu topo -->
   <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
       <div class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
-          <li><a href="#contact">Conheça nossa empresa</a></li>
+          <li><a href="#about">Sobre</a></li>
           <li><a href="contato/feedbackMail.php">Contato</a></li>
         </ul>
-
       </div><!--/.nav-collapse -->
     </div>
   </div>
-			
+
 
 
   <!-- Logo -->
   <div class="jumbotron">
     <div class="container">
-      <h1>MaisProcessos</h1>
-      <p>Sistema de monitoramento e gerência de processos</p>
+      <h1>Gerenciador de Processos</h1>
+      <p>Gerencie e acompanhe a situação dos seus processos</p>
     </div>
-  </div>	
+  </div>
 
-	<div id="login_content">
-		Usuario <input type="text" name="usuario" />
-		Senha <input type="password" name="senha" />
-		<button id="btn_login" onclick="validarSenha();" >Acessar</button> 
-	</div>
+  <div class="container theme-showcase" role="main">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+          <h1 class="text-center login-title">Entre para acessar seus processos</h1>
+          <div class="account-wall">
+            <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
+            alt="">
+            <div class="form-signin">
+              <div id="login_content">
+                <input type="text" class="form-control" placeholder="Usuário" name="usuario" required autofocus>
+                <input type="password" class="form-control" placeholder="Senha" name="senha" required>
+                <button class="btn btn-lg btn-primary btn-block" id="btn_login" onclick="validarSenha();">Entrar</button>
+                </div>
+                <label class="checkbox pull-left">
+                  <input type="checkbox" value="lembrar">
+                  Lembrar
+                </label>
+                <span class="clearfix"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>© UFCG 2014</p>
+      </footer>
+    </div> <!-- /container -->
 
 
-
-    <hr>
-
-    <footer>
-      <p>© UFCG 2014</p>
-    </footer>
-  </div> <!-- /container -->
-
-</div>
+  </body></html>
